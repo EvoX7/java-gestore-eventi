4ad9b12249b0b7a3eb664be7d13849824918f1c7
Marc <102824384+EvoX7@users.noreply.github.com>
2022-11-25 17:32:56 +0100

    Milestone 2

---
+++ src/org/generation/italy/eventi/Event.java
@@ -0,0 +1,80 @@
+package org.generation.italy.eventi;
+
+import java.time.LocalDate;
+
+public class Event {
+
+	private String title;
+	private LocalDate date;
+	private int totalSeats;
+	private int reservedSeats = 0;
+
+	public Event(String title, LocalDate date, int totalSeats) throws Exception {
+
+		setTitle(title);
+		setDate(date);
+		this.totalSeats = totalSeats;
+
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) throws Exception {
+		if (date.isBefore(LocalDate.now())) {
+			throw new Exception("Not a valid date");
+		}
+		this.date = date;
+	}
+
+	public int getTotalSeats() {
+
+		return totalSeats;
+	}
+
+	public void setTotalSeats(int totalSeats) throws Exception {
+		if (totalSeats <= 0) {
+			throw new Exception("Negative values are forbidden");
+		}
+		this.totalSeats = totalSeats;
+	}
+
+	public int getReservedSeats() {
+		return reservedSeats;
+	}
+
+//	Methods
+
+	public void bookEvent() throws Exception {
+		if (reservedSeats == totalSeats || date.isBefore(LocalDate.now())) {
+			throw new Exception("Sorry, you cannot book event/seats not available");
+		}
+		reservedSeats++;
+
+	}
+
+	public void cancelReservation() throws Exception {
+		if (reservedSeats == 0 || date.isBefore(LocalDate.now())) {
+
+			throw new Exception("System error, please insert a valid number");
+		}
+		reservedSeats--;
+
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Title: " + getTitle() + "\nDate : " + getDate();
+	}
+
+}
---
+++ src/org/generation/italy/eventi/Main.java
@@ -0,0 +1,93 @@
+package org.generation.italy.eventi;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Event event = null;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Welcome to EventBookingSystem, press ENTER to continue ");
+		System.out.println("---------------------------");
+		String enter = sc.nextLine();
+
+		System.out.print("Please insert event name: ");
+		String eventTitle = sc.nextLine();
+
+		System.out.print("Please insert date for this event (YYYY-MM-DD): ");
+		String date = sc.nextLine();
+		LocalDate dateEvent = LocalDate.parse(date);
+
+		System.out.print("Please insert alla available seats: ");
+		int seats = sc.nextInt();
+
+		try {
+			event = new Event(eventTitle, dateEvent, seats);
+
+			System.out.println("---------------------------");
+			System.out.println(event + "\n");
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+
+		}
+
+		System.out.print("Do you want to add another reservation [y/n] ");
+		String response = sc.next();
+
+		if (response.equals("y")) {
+			System.out.println("How many reservation do you want to book? ");
+			int reservedNumber = sc.nextInt();
+
+			for (int x = 0; x < reservedNumber; x++) {
+				try {
+					event.bookEvent();
+				} catch (Exception e) {
+
+					System.err.println(e.getMessage());
+				}
+				
+			}
+		} else  {
+			System.out.println("GoodBye, thank you!");
+			System.exit(1);
+
+		}
+
+		System.out.println("---------------------------");
+		int availableSeats = event.getTotalSeats() - event.getReservedSeats();
+		System.out.println("\nTotal Reserved Seats: " + event.getReservedSeats());
+		System.out.println("Total available: " + availableSeats);
+
+		System.out.println("-------------------\nDo you want to cancel reservations? [y/n] ");
+
+		String checkforResponse2 = sc.next();
+
+		if (checkforResponse2.equals("y")) {
+			System.out.println("\nHow many reservations do you want to cancel ? ");
+			int canceluserRes = sc.nextInt();
+			for (int x = 0; x < canceluserRes; x++) {
+				try {
+					event.cancelReservation();
+				} catch (Exception e) {
+
+					System.err.println(e.getMessage());
+				}
+
+			}
+		} else {
+			System.out.println("GoodBye, thank you!");
+			System.exit(1);
+			
+			System.out.println("---------------------------");
+			int availableSeats2 = event.getTotalSeats() - event.getReservedSeats();
+			System.out.println("\nTotal Reserved Seats: " + event.getReservedSeats());
+			System.out.println("Total available: " + availableSeats2);
+
+		}
+	}
+}
